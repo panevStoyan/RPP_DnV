cee55a4aa53b4ee70764ca3404a7e9f298a6f710
vanesa-ltc <x.vanesa.ltc@gmail.com>
2023-12-12 22:59:42 +0200

    worked on history, did the whole list bullshit but i can't deal with the imgs today

---
+++ Site/get_fucked.js
@@ -0,0 +1,7 @@
+document.addEventListener('copy', function (event) {
+    // Prevents copying for a specific element
+    if (event.target.id === 'protected-content') {
+        event.preventDefault();
+        alert('No copying here m8, get fucked :D');
+    }
+});
